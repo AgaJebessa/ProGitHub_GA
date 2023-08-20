931d90a424cb1f86db38f561a92c1d2825b657b4
AgaJebessa <118113193+AgaJebessa@users.noreply.github.com>
2023-08-19 22:17:17 -0400

    The hypotenuse of the triangle was calculated

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+import static java.lang.Math.round;
+
+public class Main{
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter x: ");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        double x =scanner.nextDouble();
+
+        System.out.println("Enter Y:");
+        double y= scanner.nextDouble();
+        //Calculate the hypotenuse/ e
+        float z=(float)Math.sqrt((x*x)+(y*y));
+
+        System.out.println("The hypotenuse of the triangle is " + z);
+
+
         }
     }
-}
